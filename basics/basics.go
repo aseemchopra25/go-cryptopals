6bc50ff133b1f50c358345d87ff0fab5e4ea3e10
aseemchopra25 <aseemchopra@protonmail.com>
2022-08-01 17:14:36 +0530

    :sparkles: solved Set 1 Challenge 1

--- basics/basics.go
+++ basics/basics.go
@@ -1,7 +1,27 @@
 package basics
 
-import "fmt"
+import (
+	"encoding/base64"
+	"encoding/hex"
+)
 
-func HexToB64() {
-	fmt.Println("Boilerplate")
+func HexToB64(h string) string {
+	// we convert the string to byte array
+	hb := []byte(h)
+	// calculate length of decoded hex first
+	// to create an empty byte array storing the decoded output
+	dst := make([]byte, hex.DecodedLen(len(hb)))
+	// decode the hex to bytes
+	hex.Decode(dst, hb)
+	// now dst contains hex decoded to bytes
+
+	// to convert these decoded bytes to base64
+	// concept: base64 clumbs 8 bit bytes together to form 24 bit chunks
+	// these 24 bit chunks are divided into 6 bit base64 digits
+	// = is a padding character for base64
+	// which you must've often seen
+
+	b64 := make([]byte, base64.StdEncoding.EncodedLen(len(dst)))
+	base64.StdEncoding.Encode(b64, dst)
+	return string(b64)
 }
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/aseemchopra25/go-cryptopals/basics"
+)
 
 func main() {
-	fmt.Println("Cryptopals Rocks")
+	// Challenge Set 1: Basics
+
+	// 1. Hex to Base 64
+	h := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	fmt.Println(basics.HexToB64(h))
+
 }
