10fe2a7018a4725f289457dfc7afb8d94310150b
aseemchopra25 <aseemchopra@protonmail.com>
2022-08-02 01:35:08 +0530

    :sparkles: solved Set 1 Challenge 2

--- basics/basics.go
+++ basics/basics.go
@@ -5,7 +5,7 @@ import (
 	"encoding/hex"
 )
 
-func HexToB64(h string) string {
+func HexToBytes(h string) []byte {
 	// we convert the string to byte array
 	hb := []byte(h)
 	// calculate length of decoded hex first
@@ -14,14 +14,28 @@ func HexToB64(h string) string {
 	// decode the hex to bytes
 	hex.Decode(dst, hb)
 	// now dst contains hex decoded to bytes
+	return dst
+}
+func HexToB64(h string) string {
+	dst := HexToBytes(h)
 
-	// to convert these decoded bytes to base64
-	// concept: base64 clumbs 8 bit bytes together to form 24 bit chunks
+	// Convert these decoded bytes to base64
+	// Concept: base64 clumps 8 bit bytes together to form 24 bit chunks
 	// these 24 bit chunks are divided into 6 bit base64 digits
 	// = is a padding character for base64
-	// which you must've often seen
 
 	b64 := make([]byte, base64.StdEncoding.EncodedLen(len(dst)))
 	base64.StdEncoding.Encode(b64, dst)
 	return string(b64)
+}
+
+func FixedXor(a string, b string) string {
+	a1 := HexToBytes(a)
+	b1 := HexToBytes(b)
+	ret := make([]byte, len(a1))
+	for i := 0; i < len(a1); i++ {
+		ret[i] = a1[i] ^ b1[i]
+	}
+	return hex.EncodeToString(ret)
+
 }
--- main.go
+++ main.go
@@ -9,8 +9,13 @@ import (
 func main() {
 	// Challenge Set 1: Basics
 
-	// 1. Hex to Base 64
+	fmt.Println("1. Hex to Base64")
 	h := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 	fmt.Println(basics.HexToB64(h))
+
+	fmt.Println("2. Fixed XOR")
+	a := "1c0111001f010100061a024b53535009181c"
+	b := "686974207468652062756c6c277320657965"
+	fmt.Println(basics.FixedXor(a, b))
 
 }
